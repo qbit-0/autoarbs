f8fa8b4eb40107cb83e2f5da266be5ac88a00ebd
Duy Pham <duypham12241999@gmail.com>
2022-11-26 16:23:31 -0800

    clamp deposits and withdrawals

--- autoarbs/src/components/Withdraw.tsx
+++ autoarbs/src/components/Withdraw.tsx
@@ -1,11 +1,21 @@
-import { useEffect } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 
 type Props = {};
 
+const balance = 1000;
+
 const WithdrawCard = (props: Props) => {
+  const [amount, setAmount] = useState(0);
+
   useEffect(() => {
     M.updateTextFields();
   }, []);
+
+  const handleChangeAmount = (e: ChangeEvent<HTMLInputElement>) => {
+    const parsedNum = Number.parseFloat(e.currentTarget.value);
+    const newAmount = Math.min(Math.max(parsedNum, 0), balance);
+    setAmount(newAmount);
+  };
 
   return (
     <div className="col s12 m6">
@@ -14,7 +24,15 @@ const WithdrawCard = (props: Props) => {
           <div className="card-content">
             <span className="card-title">Withdraw</span>
             <div className="input-field">
-              <input id="amount" type="number" className="validate" />
+              <input
+                className="validate"
+                id="amount"
+                type="number"
+                min={0}
+                max={balance}
+                value={amount}
+                onChange={handleChangeAmount}
+              />
               <label htmlFor="amount">Amount</label>
             </div>
           </div>

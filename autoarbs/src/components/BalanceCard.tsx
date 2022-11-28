feea5d85fe58e93e6ca1e18b41fbc9b760740a99
Duy Pham <duypham12241999@gmail.com>
2022-11-28 15:24:06 -0800

    Add more info to balance card

--- autoarbs/src/components/BalanceCard.tsx
+++ autoarbs/src/components/BalanceCard.tsx
@@ -1,17 +1,47 @@
 import React from "react";
 import { useAppSelector } from "../app/hooks";
-import { selectBalance } from "../features/account/accountSlice";
+import {
+  selectBalance,
+  selectDeposited,
+  selectWithdrawn,
+} from "../features/account/accountSlice";
 
 type Props = {};
 
 const BalanceCard = (props: Props) => {
   const balance = useAppSelector(selectBalance);
+  const deposited = useAppSelector(selectDeposited);
+  const withdrawn = useAppSelector(selectWithdrawn);
+
+  const profit = balance + withdrawn - deposited;
+  const profitPercent = (profit / deposited) * 100;
 
   return (
     <div className="card hoverable">
       <div className="card-content">
         <span className="card-title">Balance</span>
-        <h2>${balance}</h2>
+        <div className="row">
+          <div className="col s12">
+            <h3>${balance}</h3>
+          </div>
+        </div>
+        <div className="row divider" />
+        <div className="row">
+          <div className="col s12 m4">
+            <p>Total Deposited</p>
+            <h3>${deposited}</h3>
+          </div>
+          <div className="col s12 m4">
+            <p>Total Withdrawn</p>
+            <h3>${withdrawn}</h3>
+          </div>
+          <div className="col s12 m4">
+            <p>Total Profit</p>
+            <h3>
+              ${profit} ({profitPercent}%)
+            </h3>
+          </div>
+        </div>
       </div>
     </div>
   );
--- autoarbs/src/features/account/accountSlice.ts
+++ autoarbs/src/features/account/accountSlice.ts
@@ -3,9 +3,15 @@ import { RootState } from "../../app/store";
 
 export type AccountState = {
   balance: number;
+  deposited: number;
+  withdrawn: number;
 };
 
-const initialState = { balance: 9999.99 };
+const initialState = {
+  balance: 30,
+  deposited: 20,
+  withdrawn: 10,
+};
 
 const accountSlice = createSlice({
   name: "account",
@@ -13,9 +19,11 @@ const accountSlice = createSlice({
   reducers: {
     deposit: (state, action) => {
       state.balance += action.payload;
+      state.deposited += action.payload;
     },
     withdraw: (state, action) => {
       state.balance -= action.payload;
+      state.withdrawn += action.payload;
     },
   },
 });
@@ -23,5 +31,7 @@ const accountSlice = createSlice({
 export const accountActions = accountSlice.actions;
 
 export const selectBalance = (state: RootState) => state.account.balance;
+export const selectDeposited = (state: RootState) => state.account.deposited;
+export const selectWithdrawn = (state: RootState) => state.account.withdrawn;
 
 export default accountSlice.reducer;

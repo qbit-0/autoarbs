e306ab9ab2472194ad1d36688a24f3a303006534
Duy Pham <duypham12241999@gmail.com>
2022-11-28 16:55:08 -0800

    Put account settings inside form

--- autoarbs/src/components/cards/AccountSettingsCard.tsx
+++ autoarbs/src/components/cards/AccountSettingsCard.tsx
@@ -3,24 +3,26 @@ type Props = {};
 const AccountSettingsCard = (props: Props) => {
   return (
     <div className="card hoverable">
-      <div className="card-content">
-        <div className="card-title">Settings</div>
-        <div className="input-field">
-          <select title="Example Setting" value="1">
-            <option value="1">Option 1</option>
-            <option value="2">Option 2</option>
-            <option value="3">Option 3</option>
-          </select>
-          <label>Example Setting</label>
+      <form>
+        <div className="card-content">
+          <div className="card-title">Settings</div>
+          <div className="input-field">
+            <select title="Example Setting" value="1">
+              <option value="1">Option 1</option>
+              <option value="2">Option 2</option>
+              <option value="3">Option 3</option>
+            </select>
+            <label>Example Setting</label>
+          </div>
         </div>
-      </div>
-      <div className="card-action">
-        <div className="flex justify-end">
-          <button className="btn waves-effect waves-light" type="submit">
-            Save
-          </button>
+        <div className="card-action">
+          <div className="flex justify-end">
+            <button className="btn waves-effect waves-light" type="submit">
+              Save
+            </button>
+          </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 };

64eb7ea1e7dc2a3994d25a1c80efaa12cbb81e12
Duy Pham <duypham12241999@gmail.com>
2022-12-04 12:28:14 -0800

    Add cards on landing page to create account or login

--- autoarbs/src/components/pages/LandingPage.tsx
+++ autoarbs/src/components/pages/LandingPage.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 type Props = {};
 
 const LandingPage = (props: Props) => {
@@ -9,6 +11,46 @@ const LandingPage = (props: Props) => {
             <h1>AutoArbs</h1>
             <h2>Put Money In, Get Money Out. It's That Simple.</h2>
             <p>This is where we explain the app.</p>
+          </div>
+          <div className="row">
+            <div className="col s12 m6">
+              <div className="card hoverable">
+                <div>
+                  <div className="card-content">
+                    <span className="card-title">First time investor?</span>
+                    <p>We make it easy.</p>
+                  </div>
+                  <div className="card-action">
+                    <div className="flex justify-center">
+                      <Link to="/registration">
+                        <button className="btn waves-effect waves-light">
+                          Create an Account
+                        </button>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col s12 m6">
+              <div className="card hoverable">
+                <div>
+                  <div className="card-content">
+                    <span className="card-title">Already have an account?</span>
+                    <p>Let's see your profits.</p>
+                  </div>
+                  <div className="card-action">
+                    <div className="flex justify-center">
+                      <Link to="/login">
+                        <button className="btn waves-effect waves-light">
+                          Login
+                        </button>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>

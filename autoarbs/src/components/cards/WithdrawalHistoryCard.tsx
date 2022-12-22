7cce893cbdc806966cfa7182ac313f1dd25393a4
Duy Pham <duypham12241999@gmail.com>
2022-12-22 12:33:28 -0800

    Make sort logic more clear

--- autoarbs/src/components/cards/WithdrawalHistoryCard.tsx
+++ autoarbs/src/components/cards/WithdrawalHistoryCard.tsx
@@ -19,10 +19,10 @@ const WithdrawalHistoryCard = (props: Props) => {
     .sort((a, b) => {
       const aDate = new Date(a.createdAt);
       const bDate = new Date(b.createdAt);
-      if (aDate < bDate) {
-        return 1;
-      } else if (aDate > bDate) {
+      if (aDate > bDate) {
         return -1;
+      } else if (aDate < bDate) {
+        return 1;
       } else {
         return 0;
       }

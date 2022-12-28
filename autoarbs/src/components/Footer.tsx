29fbfd26bb31503f15fd0596bbb63090cf3fc804
Duy Pham <duypham12241999@gmail.com>
2022-12-28 15:44:17 -0800

    Update footer

--- autoarbs/src/components/Footer.tsx
+++ autoarbs/src/components/Footer.tsx
@@ -1,14 +1,23 @@
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Container, Typography, useTheme } from "@mui/material";
 
 type Props = {};
 
 const Footer = (props: Props) => {
+  const theme = useTheme();
+
   return (
-    <Box component="footer" bgcolor="lightblue">
-      <Container>
-        <Typography variant="h5">Footer Heading</Typography>
-        <Typography>Footer Text</Typography>
-      </Container>
+    <Box component="footer" bgcolor={theme.palette.primary.main}>
+      <Box py={2}>
+        <Container>
+          <Typography variant="h5" color="white">
+            AutoArbs
+          </Typography>
+          <Typography color="white">
+            We bring arbitrage sports-betting to everyone. Guaranteed profits,
+            withdraw anytime.
+          </Typography>
+        </Container>
+      </Box>
     </Box>
   );
 };
